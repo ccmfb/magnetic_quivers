b5c6ea1208c885385d11e9cd744ef485ea99e2db
Carl <carl.c.b@icloud.com>
2025-06-24 15:55:49 +0100

    added D7 contribution to ~intersection number~

--- brane_webs.py
+++ brane_webs.py
@@ -5,15 +5,57 @@ from shapely.strtree import STRtree
 from typing import List, Optional, Tuple
 
 
+class D7Brane:
+    '''
+    D7 brane which acts as end point for other branes.
+    '''
+
+    def __init__(self):
+        self.branes_inside: List[Brane] = []
+        self.branes_outside: List[Brane] = []
+
+    def add_brane_inside(self, brane: 'Brane'):
+        '''
+        Add a brane to the inside of this D7 brane.
+        '''
+        self.branes_inside.append(brane)
+
+    def add_brane_outside(self, brane: 'Brane'):
+        '''
+        Add a brane to the outside of this D7 brane.
+        '''
+        self.branes_outside.append(brane)
+
+    def __hash__(self) -> int:
+        # Hash by identity so D7Brane can be used in sets/dicts
+        return id(self)
+
+    def __eq__(self, other: object) -> bool:
+        # Equal only if same object
+        return self is other
+
+
 class Brane:
     '''
     A single brane represented as a line segment in 2D, determined by a start point,
     a (p, q) charge vector, and a length. All inputs are numpy arrays for consistency.
     '''
 
-    def __init__(self, pos1: np.ndarray, pq_charge: np.ndarray, length: float):
+    def __init__(
+            self, 
+            pos1: np.ndarray, 
+            pq_charge: np.ndarray, 
+            length: float, 
+            starts_on: D7Brane = None,
+            starts_on_side: str = None,
+            ends_on: D7Brane = None,
+            ends_on_side: str = None
+        ):
+
         assert pq_charge[0] != 0 or pq_charge[1] != 0, "Brane must have a non-zero (p, q) charge."
         assert length > 0, "Length must be a positive number."
+        assert starts_on_side in [None, 'inside', 'outside'], "Invalid start side specification."
+        assert ends_on_side in [None, 'inside', 'outside'], "Invalid end side specification."
 
         self.length = length
         self.pq_charge = np.asarray(pq_charge, dtype=int)
@@ -23,6 +65,25 @@ class Brane:
 
         self.line = LineString([self.pos1, self.pos2])
 
+        self.starts_on = starts_on
+        self.starts_on_side = starts_on_side
+
+        self.ends_on = ends_on
+        self.ends_on_side = ends_on_side
+
+        # Adding this brane to the D7 branes on which it starts/ends
+        if starts_on is not None:
+            if starts_on_side == 'inside':
+                starts_on.add_brane_inside(self)
+            if starts_on_side == 'outside':
+                starts_on.add_brane_outside(self)
+        
+        if ends_on is not None:
+            if ends_on_side == 'inside':
+                ends_on.add_brane_inside(self)
+            if ends_on_side == 'outside':
+                ends_on.add_brane_outside(self)
+
     def plot(self, ax: Optional[plt.Axes] = None, **kwargs):
         '''
         Plot this brane segment on the given Axes.
@@ -31,7 +92,28 @@ class Brane:
             ax = plt.gca()
 
         xs, ys = self.line.xy
-        ax.plot(xs, ys, **kwargs)
+        line_obj = ax.plot(xs, ys, **kwargs)
+        
+        if self.starts_on is not None:
+            ax.scatter(*self.pos1, color='black', label='Start', s=20, zorder=5)
+
+        if self.ends_on is not None:
+            ax.scatter(*self.pos2, color='black', label='End', s=20, zorder=5)
+
+        mid = (self.pos1 + self.pos2) / 2.0
+        dir_vec = self.pos2 - self.pos1
+        perp = np.array([-dir_vec[1], dir_vec[0]])
+
+        if np.linalg.norm(perp) > 0:
+            perp = perp / np.linalg.norm(perp) * 0.05 * self.length
+
+        label_pos = mid + perp
+        color = 'black'
+
+        ax.text(label_pos[0], label_pos[1],
+                f"({self.pq_charge[0]},{self.pq_charge[1]})",
+                fontsize=9, color=color,
+                ha='center', va='center')
 
         return ax
 
@@ -96,7 +178,52 @@ class BraneWeb:
                     # if brane1.pq_charge[0] * brane2.pq_charge[1] - brane1.pq_charge[1] * brane2.pq_charge[0] != 0:
                     count += np.abs(brane1.pq_charge[0] * brane2.pq_charge[1] - brane1.pq_charge[1] * brane2.pq_charge[0])
 
+        d7_branes1 = self.get_D7_branes()
+        d7_branes2 = other.get_D7_branes()
+        shared_d7_branes = set(d7_branes1) & set(d7_branes2)
+
+        for d7_brane in shared_d7_branes:
+            count_inside_web1 = 0
+            count_outside_web1 = 0
+            count_inside_web2 = 0
+            count_outside_web2 = 0
+
+            print('shared: ', d7_brane)
+            for brane in self.branes:
+                if (brane.starts_on == d7_brane and brane.starts_on_side == 'inside') or (brane.ends_on == d7_brane and brane.ends_on_side == 'inside'):
+                    count_inside_web1 += 1
+                if (brane.starts_on == d7_brane and brane.starts_on_side == 'outside') or (brane.ends_on == d7_brane and brane.ends_on_side == 'outside'):
+                    count_outside_web1 += 1
+
+            for brane in other.branes:
+                if (brane.starts_on == d7_brane and brane.starts_on_side == 'inside') or (brane.ends_on == d7_brane and brane.ends_on_side == 'inside'):
+                    count_inside_web2 += 1
+                if (brane.starts_on == d7_brane and brane.starts_on_side == 'outside') or (brane.ends_on == d7_brane and brane.ends_on_side == 'outside'):
+                    count_outside_web2 += 1
+
+            # Each pair of inside/outside counts contributes to the intersection number
+            count += count_inside_web1 * count_outside_web2
+            count += count_inside_web2 * count_outside_web1
+            count -= count_inside_web1 * count_inside_web2
+            count -= count_outside_web1 * count_outside_web2
+
         return count
+
+    def get_D7_branes(self) -> List[D7Brane]:
+        '''
+        Returns a list of all D7 branes that are start/end points of branes in this web.
+        '''
+        d7_branes = []
+        for brane in self.branes:
+            starts_on = brane.starts_on
+            ends_on = brane.ends_on
+
+            if starts_on is not None and starts_on not in d7_branes:
+                d7_branes.append(starts_on)
+            if ends_on is not None and ends_on not in d7_branes:
+                d7_branes.append(ends_on)
+        
+        return d7_branes
     
     def plot(self, ax: Optional[plt.Axes] = None, **kwargs):
         '''
