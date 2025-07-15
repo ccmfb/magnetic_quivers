2b41e296323bf5ef1b9236ca8e7a073719d1e339
Carl <carl.c.b@icloud.com>
2025-07-15 11:53:48 +0100

    added remaining simply laced min transitions

--- unitary_quiver.py
+++ unitary_quiver.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from pyvis.network import Network
 from collections import deque, defaultdict
 from typing import Dict, List
+import numpy as np
 
 
 class Quiver:
@@ -243,43 +244,79 @@ class Quiver:
 def init_minimal_transitions(dim_upper_bound: int = 10):
     '''Some minimal nilpotent orbit transitions... CLEAN THIS, INDICES ARE TOO MESY'''
 
-    an_quivers = []
+    minimal_transitions = []
+
+    # a_n transitions
+    # ------------------------
     an_upper_bound = dim_upper_bound
-    for n in range(1, an_upper_bound + 1):
+    for n in range(2, an_upper_bound + 1):
         nodes = [i+1 for i in range(n+1)]
         linear_edges = [(i, i+1) for i in range(1, n)]
         all_edges = linear_edges + [(n, n+1)] + [(n+1, 1)]
         values = [1] * (n + 1)
 
-        an_quivers.append(
+        minimal_transitions.append(
             Quiver(nodes, all_edges, values, name=f"a_{n}")
         )
 
-    dn_quivers = []
-    #dn_upper_bound = (dim_upper_bound + 3) / 2 if (dim_upper_bound + 3) % 2 == 0 else (dim_upper_bound + 2) / 2
-    dn_upper_bound = 5
-    for n in range(4, dn_upper_bound + 1):
+    # d_n transitions
+    # ------------------------
+    dn_upper_bound = np.ceil((dim_upper_bound + 3) / 2)
+    for n in range(4, int(dn_upper_bound) + 1):
         nodes = [i+1 for i in range(n+1)]
         linear_edges = [(i, i+1) for i in range(1, n-1)]
         all_edges = linear_edges + [(2, n)] + [(n-2, n+1)]
         values = [1] + [2] * (n - 3) + [1] + [1, 1]
 
-        dn_quivers.append(
+        minimal_transitions.append(
             Quiver(nodes, all_edges, values, name=f"d_{n}")
         )
 
-    An_quivers = []
+    # A_n transitions
+    # ------------------------
     An_upper_bound = dim_upper_bound
     for n in range(2, An_upper_bound+1):
         nodes = [1, 2]
         edges = [(1,2) for _ in range(n)]
         values = [1, 1]
 
-        An_quivers.append(
+        minimal_transitions.append(
             Quiver(nodes, edges, values, name=f'A_{n}')
         )
 
-    return an_quivers + dn_quivers + An_quivers
+    # e_6 transition
+    # ------------------------
+    if dim_upper_bound >= 11:
+        nodes = [1, 2, 3, 4, 5, 6 , 7]
+        edges = [
+            (1,2), (2,3), (3,4), (4,5), (3,6), (6,7)
+        ]
+        values = [1, 2, 3, 2, 1, 2, 1]
+
+        minimal_transitions.append(
+            Quiver(nodes, edges, values, name='e_6')
+        )
+
+    if dim_upper_bound >= 17:
+        nodes = [i+1 for i in range(8)]
+        edges = [(i+1, i+2) for i in range(6)] + [(4,8)]
+        values = [1, 2, 3, 4, 3, 2, 1, 2]
+
+        minimal_transitions.append(
+            Quiver(nodes, edges, values, name='e_7')
+        )
+
+    if dim_upper_bound >= 29:
+        nodes = [i+1 for i in range(9)]
+        edges = [(i+1, i+2) for i in range(7)] + [(6,9)]
+        values = [1, 2, 3, 4, 5, 6, 4, 2, 3]
+        
+        minimal_transitions.append(
+            Quiver(nodes, edges, values, name='e_8')
+        )
+    
+
+    return minimal_transitions
     #return an_quivers, dn_quivers
 
 
